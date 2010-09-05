c0989229e5d87c5799df8358642cc4450f7696f3
Wesley Lima <wesleyskap@gmail.com>
2010-09-05 10:15:30 -0300

    feat: implement base error hierarchy for execution failures

---
+++ lib/squall/errors.rb
@@ -0,0 +1,7 @@
+﻿module Squall
+  class Error < StandardError; end
+  class ExecutionError < Error; end
+  class TimeoutError < Error; end
+  class CancelledError < Error; end
+  class WorkerCrashError < Error; end
+end
