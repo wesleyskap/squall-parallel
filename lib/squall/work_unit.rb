8442e5b29f5757b76de65c9b4147c7c5a97e7a19
Wesley Lima <wesleyskap@gmail.com>
2010-09-19 11:35:00 -0300

    feat: implement WorkUnit value object for item payload and index tracking

---
+++ lib/squall/work_unit.rb
@@ -0,0 +1,14 @@
+﻿module Squall
+  class WorkUnit
+    attr_reader :item, :index
+
+    def initialize(item, index)
+      @item = item
+      @index = index
+    end
+
+    def process_with(block)
+      block.call(item)
+    end
+  end
+end
