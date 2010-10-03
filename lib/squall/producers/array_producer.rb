c0ea537e1f6002793c9fa898d3faf5abd0afa3bb
Wesley Lima <wesleyskap@gmail.com>
2010-10-03 16:40:15 -0300

    feat: implement ArrayProducer adapter for standard collections

---
+++ lib/squall/producers/array_producer.rb
@@ -0,0 +1,21 @@
+﻿module Squall
+  module Producers
+    class ArrayProducer
+      def initialize(collection)
+        @collection = collection.to_a
+      end
+
+      def each_unit
+        return enum_for(:each_unit) unless block_given?
+
+        @collection.each_with_index do |item, idx|
+          yield WorkUnit.new(item, idx)
+        end
+      end
+
+      def size
+        @collection.size
+      end
+    end
+  end
+end
