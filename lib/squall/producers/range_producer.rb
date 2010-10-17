fc932fa8c288278702d98400ca53d9c2a7f4782c
Wesley Lima <wesleyskap@gmail.com>
2010-10-17 09:20:45 -0300

    feat: implement RangeProducer adapter for numeric sequences

---
+++ lib/squall/producers/range_producer.rb
@@ -0,0 +1,23 @@
+﻿module Squall
+  module Producers
+    class RangeProducer
+      def initialize(range)
+        @range = range
+      end
+
+      def each_unit
+        return enum_for(:each_unit) unless block_given?
+
+        idx = 0
+        @range.each do |item|
+          yield WorkUnit.new(item, idx)
+          idx += 1
+        end
+      end
+
+      def size
+        @range.size
+      end
+    end
+  end
+end
