d3d2901c1edf87ceb92f86eb0d8b12f47aee6254
Wesley Lima <wesleyskap@gmail.com>
2010-11-07 14:15:20 -0300

    feat: declare BaseExecutor contract defining workers count and execution interface

---
+++ lib/squall/executors/base_executor.rb
@@ -0,0 +1,15 @@
+﻿module Squall
+  module Executors
+    class BaseExecutor
+      attr_reader :workers_count
+
+      def initialize(workers_count = 4)
+        @workers_count = workers_count
+      end
+
+      def execute(producer, &block)
+        raise NotImplementedError, "#{self.class} must implement #execute"
+      end
+    end
+  end
+end
