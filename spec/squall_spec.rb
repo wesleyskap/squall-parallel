bf687f144c12c68a2b44ad4d05d25a1a3537f5a4
Wesley Lima <wesleyskap@gmail.com>
2011-01-09 10:45:20 -0300

    test: add unit tests for thread executor array and range processing

---
+++ spec/squall_spec.rb
@@ -0,0 +1,16 @@
+﻿require "spec_helper"
+
+RSpec.describe Squall do
+  describe ".map" do
+    it "maps array items concurrently using threads" do
+      input = [1, 2, 3, 4, 5]
+      results = Squall.map(input, in_threads: 2) { |n| n * 2 }
+      expect(results).to eq([2, 4, 6, 8, 10])
+    end
+
+    it "maps range inputs correctly" do
+      results = Squall.map(1..5, in_threads: 2) { |n| n + 1 }
+      expect(results).to eq([2, 3, 4, 5, 6])
+    end
+  end
+end
