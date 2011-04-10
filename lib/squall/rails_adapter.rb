84913a1c2f2ace7ea6b74bfc04a97c11b6c55ee3
Wesley Lima <wesleyskap@gmail.com>
2011-04-10 09:30:15 -0300

    feat: implement RailsAdapter for ActiveRecord connection recovery post-fork

---
+++ lib/squall/rails_adapter.rb
@@ -0,0 +1,17 @@
+﻿module Squall
+  class RailsAdapter
+    def self.establish_fork_connection
+      if defined?(::ActiveRecord::Base)
+        ::ActiveRecord::Base.establish_connection
+      end
+    end
+
+    def self.with_connection(&block)
+      if defined?(::ActiveRecord::Base) && ::ActiveRecord::Base.respond_to?(:connection_pool)
+        ::ActiveRecord::Base.connection_pool.with_connection(&block)
+      else
+        yield
+      end
+    end
+  end
+end
---
+++ spec/rails_adapter_spec.rb
@@ -0,0 +1,9 @@
+﻿require "spec_helper"
+
+RSpec.describe Squall::RailsAdapter do
+  it "gracefully yields when ActiveRecord is not defined" do
+    yielded = false
+    Squall::RailsAdapter.with_connection { yielded = true }
+    expect(yielded).to be true
+  end
+end
