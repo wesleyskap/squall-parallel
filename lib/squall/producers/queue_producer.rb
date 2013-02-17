ae17bb72db79a079152f792fdc2688300dcb1757
Wesley Lima <wesleyskap@gmail.com>
2013-02-17 10:15:30 -0300

    feat: add QueueProducer supporting Thread::Queue streams for Ruby 2.0

---
+++ lib/squall/producers/queue_producer.rb
@@ -0,0 +1,26 @@
+﻿module Squall
+  module Producers
+    class QueueProducer
+      def initialize(queue)
+        @queue = queue
+      end
+
+      def each_unit
+        return enum_for(:each_unit) unless block_given?
+
+        idx = 0
+        until @queue.empty?
+          item = @queue.pop(true) rescue nil
+          break if item.nil?
+
+          yield WorkUnit.new(item, idx)
+          idx += 1
+        end
+      end
+
+      def size
+        @queue.size
+      end
+    end
+  end
+end
