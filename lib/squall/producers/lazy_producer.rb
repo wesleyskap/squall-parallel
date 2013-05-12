2cfc1b8d77d7ed3dd21fb03383451ae7e0274bc3
Wesley Lima <wesleyskap@gmail.com>
2013-05-12 14:30:10 -0300

    feat: add LazyProducer supporting Enumerable::Lazy evaluation

---
+++ lib/squall/producers/lazy_producer.rb
@@ -0,0 +1,23 @@
+﻿module Squall
+  module Producers
+    class LazyProducer
+      def initialize(enum)
+        @enum = enum
+      end
+
+      def each_unit
+        return enum_for(:each_unit) unless block_given?
+
+        idx = 0
+        @enum.each do |item|
+          yield WorkUnit.new(item, idx)
+          idx += 1
+        end
+      end
+
+      def size
+        @enum.respond_to?(:size) ? @enum.size : nil
+      end
+    end
+  end
+end
