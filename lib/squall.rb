1ec7ecc5e7f3081827d57f61676b1577e8874c77
Wesley Lima <wesleyskap@gmail.com>
2013-08-18 11:20:00 -0300

    feat: implement each_with_index and flat_map helpers

--- lib/squall.rb
+++ lib/squall.rb
@@ -4,19 +4,53 @@ require_relative "squall/work_unit"
 require_relative "squall/rails_adapter"
 require_relative "squall/producers/array_producer"
 require_relative "squall/producers/range_producer"
+require_relative "squall/producers/queue_producer"
+require_relative "squall/producers/lazy_producer"
 require_relative "squall/executors/base_executor"
 require_relative "squall/executors/thread_executor"
 require_relative "squall/executors/process_executor"
 
 module Squall
   def self.map(collection, options = {}, &block)
-    producer = collection.is_a?(Range) ? Producers::RangeProducer.new(collection) : Producers::ArrayProducer.new(collection)
-    executor = options[:in_processes] ? Executors::ProcessExecutor.new(options[:in_processes]) : Executors::ThreadExecutor.new(options[:in_threads] || 4)
+    producer = wrap_producer(collection)
+    executor = build_executor(options)
     executor.execute(producer, &block)
   end
 
   def self.each(collection, options = {}, &block)
     map(collection, options, &block)
     collection
+  end
+
+  def self.each_with_index(collection, options = {}, &block)
+    producer = wrap_producer(collection)
+    executor = build_executor(options)
+    executor.execute(producer) { |item| block.call(item, producer.to_a.index(item)) }
+  end
+
+  def self.flat_map(collection, options = {}, &block)
+    map(collection, options, &block).flatten(1)
+  end
+
+  private
+
+  def self.wrap_producer(collection)
+    if collection.is_a?(Range)
+      Producers::RangeProducer.new(collection)
+    elsif defined?(::Queue) && collection.is_a?(::Queue)
+      Producers::QueueProducer.new(collection)
+    elsif collection.respond_to?(:lazy) && collection.is_a?(Enumerator::Lazy)
+      Producers::LazyProducer.new(collection)
+    else
+      Producers::ArrayProducer.new(collection)
+    end
+  end
+
+  def self.build_executor(options)
+    if options[:in_processes]
+      Executors::ProcessExecutor.new(options[:in_processes])
+    else
+      Executors::ThreadExecutor.new(options[:in_threads] || 4)
+    end
   end
 end
