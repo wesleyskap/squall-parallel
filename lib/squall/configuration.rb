c4fbc1ed46d8d97a0b3414884f21cab37a4d032c
Wesley Lima <wesleyskap@gmail.com>
2014-03-09 09:30:20 -0300

    feat: add Configuration class for global worker and timeout defaults

---
+++ lib/squall/configuration.rb
@@ -0,0 +1,22 @@
+﻿require "etc"
+
+module Squall
+  class Configuration
+    attr_accessor :default_workers, :default_chunk_size, :reconnect_activerecord, :timeout
+
+    def initialize
+      @default_workers = default_processor_count
+      @default_chunk_size = 1
+      @reconnect_activerecord = true
+      @timeout = nil
+    end
+
+    private
+
+    def default_processor_count
+      Etc.respond_to?(:nprocessors) ? Etc.nprocessors : 4
+    rescue StandardError
+      4
+    end
+  end
+end
