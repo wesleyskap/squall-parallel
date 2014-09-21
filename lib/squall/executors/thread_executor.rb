b082b5b62ee0086179de70659de9a976f5bbca1d
Wesley Lima <wesleyskap@gmail.com>
2014-09-21 11:25:30 -0300

    refactor: refactor ThreadExecutor to use dynamic work queue for load balancing

--- lib/squall/executors/thread_executor.rb
+++ lib/squall/executors/thread_executor.rb
@@ -5,19 +5,33 @@ module Squall
     class ThreadExecutor < BaseExecutor
       def execute(producer, &block)
         results = []
+        queue = build_work_queue(producer)
         mutex = Mutex.new
-        threads = []
-        units = producer.each_unit.to_a
-        chunk_size = (units.size.to_f / workers_count).ceil
 
-        units.each_slice(chunk_size) do |slice|
-          threads << Thread.new do
-            res = slice.map { |u| [u.index, u.process_with(block)] }
-            mutex.synchronize { results.concat(res) }
-          end
+        threads = Array.new(workers_count) do
+          Thread.new { process_queue(queue, results, mutex, &block) }
         end
         threads.each(&:join)
+
         results.sort_by { |idx, _| idx }.map { |_, val| val }
+      end
+
+      private
+
+      def build_work_queue(producer)
+        queue = Queue.new
+        producer.each_unit { |unit| queue.push(unit) }
+        queue
+      end
+
+      def process_queue(queue, results, mutex, &block)
+        until queue.empty?
+          unit = queue.pop(true) rescue nil
+          break if unit.nil?
+
+          res = RailsAdapter.with_connection { unit.process_with(block) }
+          mutex.synchronize { results << [unit.index, res] }
+        end
       end
     end
   end
