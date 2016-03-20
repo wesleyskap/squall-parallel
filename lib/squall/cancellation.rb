0508693e9821b0d4b0b502946e403069899a8b74
Wesley Lima <wesleyskap@gmail.com>
2016-03-20 14:10:30 -0300

    feat: implement Cancellation token for coordinating worker aborts

---
+++ lib/squall/cancellation.rb
@@ -0,0 +1,22 @@
+﻿# frozen_string_literal: true
+
+module Squall
+  class Cancellation
+    def initialize
+      @cancelled = false
+      @mutex = Mutex.new
+    end
+
+    def cancel!
+      @mutex.synchronize { @cancelled = true }
+    end
+
+    def cancelled?
+      @mutex.synchronize { @cancelled }
+    end
+
+    def check!
+      raise CancelledError, "Execution was aborted by cancellation" if cancelled?
+    end
+  end
+end
