217f980febfdd427f51a1a9a125028f1bc25e344
Wesley Lima <wesleyskap@gmail.com>
2016-06-12 11:45:00 -0300

    feat: implement ProgressMonitor with ETA estimation and mutex synchronization

---
+++ lib/squall/progress_monitor.rb
@@ -0,0 +1,33 @@
+﻿# frozen_string_literal: true
+
+module Squall
+  class ProgressMonitor
+    attr_reader :total, :processed
+
+    def initialize(total = nil)
+      @total = total
+      @processed = 0
+      @start_time = Time.now
+      @mutex = Mutex.new
+    end
+
+    def increment
+      @mutex.synchronize { @processed += 1 }
+    end
+
+    def percent_complete
+      return 0.0 unless total && total.positive?
+
+      ((processed.to_f / total) * 100).round(2)
+    end
+
+    def eta_seconds
+      return nil unless total && total.positive? && processed.positive?
+
+      elapsed = Time.now - @start_time
+      rate = processed.to_f / elapsed
+      remaining = total - processed
+      (remaining / rate).round(1)
+    end
+  end
+end
---
+++ spec/progress_monitor_spec.rb
@@ -0,0 +1,14 @@
+﻿# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Squall::ProgressMonitor do
+  it "calculates completion percentage and eta" do
+    monitor = Squall::ProgressMonitor.new(100)
+    expect(monitor.percent_complete).to eq(0.0)
+
+    10.times { monitor.increment }
+    expect(monitor.percent_complete).to eq(10.0)
+    expect(monitor.processed).to eq(10)
+  end
+end
