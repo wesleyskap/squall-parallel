7e97625d7ea93affea95bb08ad7b5bcd0ef99e88
Wesley Lima <wesleyskap@gmail.com>
2017-03-19 14:05:15 -0300

    refactor: integrate Cancellation and ProgressMonitor into BaseExecutor contract

--- lib/squall/executors/base_executor.rb
+++ lib/squall/executors/base_executor.rb
@@ -1,14 +1,28 @@
-﻿module Squall
+﻿# frozen_string_literal: true
+
+module Squall
   module Executors
     class BaseExecutor
-      attr_reader :workers_count
+      attr_reader :workers_count, :progress_monitor, :cancellation
 
-      def initialize(workers_count = 4)
+      def initialize(workers_count: 4, progress_monitor: nil, cancellation: nil)
         @workers_count = workers_count
+        @progress_monitor = progress_monitor
+        @cancellation = cancellation || Cancellation.new
       end
 
       def execute(producer, &block)
         raise NotImplementedError, "#{self.class} must implement #execute"
+      end
+
+      protected
+
+      def record_progress
+        @progress_monitor&.increment
+      end
+
+      def check_cancellation!
+        @cancellation.check!
       end
     end
   end
