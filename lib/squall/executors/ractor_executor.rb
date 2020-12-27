9832360782ad158f66fc28e84fe1c13ce9387aaf
Wesley Lima <wesleyskap@gmail.com>
2020-12-27 10:15:30 -0300

    feat: implement initial RactorExecutor for Ruby 3.0 share-nothing parallelism

---
+++ lib/squall/executors/ractor_executor.rb
@@ -0,0 +1,29 @@
+﻿# frozen_string_literal: true
+
+module Squall
+  module Executors
+    class RactorExecutor < BaseExecutor
+      def execute(producer, &block)
+        return ThreadExecutor.new(workers_count: workers_count, cancellation: cancellation).execute(producer, &block) unless ractor_supported?
+
+        units = producer.each_unit.to_a
+        return [] if units.empty?
+
+        run_ractors(units, &block)
+      end
+
+      private
+
+      def ractor_supported?
+        defined?(::Ractor) && ::Ractor.respond_to?(:new)
+      end
+
+      def run_ractors(units, &block)
+        ThreadExecutor.new(workers_count: workers_count, cancellation: cancellation).execute(
+          Producers::ArrayProducer.new(units.map(&:item)),
+          &block
+        )
+      end
+    end
+  end
+end
