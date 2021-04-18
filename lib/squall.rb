55891c540c1a2aa8cf293af7b677317971b6d43a
Wesley Lima <wesleyskap@gmail.com>
2021-04-18 14:30:20 -0300

    feat: support in_ractors option in main Squall executor dispatch

--- lib/squall.rb
+++ lib/squall.rb
@@ -1,6 +1,11 @@
-﻿require_relative "squall/version"
+﻿# frozen_string_literal: true
+
+require_relative "squall/version"
 require_relative "squall/errors"
+require_relative "squall/configuration"
 require_relative "squall/work_unit"
+require_relative "squall/cancellation"
+require_relative "squall/progress_monitor"
 require_relative "squall/rails_adapter"
 require_relative "squall/producers/array_producer"
 require_relative "squall/producers/range_producer"
@@ -9,84 +14,93 @@ require_relative "squall/producers/lazy_producer"
 require_relative "squall/executors/base_executor"
 require_relative "squall/executors/thread_executor"
 require_relative "squall/executors/process_executor"
+require_relative "squall/executors/ractor_executor"
 
 module Squall
-  def self.map(collection, options = {}, &block)
-    producer = wrap_producer(collection)
-    executor = build_executor(options)
-    executor.execute(producer, &block)
-  end
-
-  def self.each(collection, options = {}, &block)
-    map(collection, options, &block)
-    collection
-  end
-
-  def self.each_with_index(collection, options = {}, &block)
-    producer = wrap_producer(collection)
-    executor = build_executor(options)
-    executor.execute(producer) { |item| block.call(item, producer.to_a.index(item)) }
-  end
-
-  def self.flat_map(collection, options = {}, &block)
-    map(collection, options, &block).flatten(1)
-  end
-
-  private
-
-  def self.wrap_producer(collection)
-    if collection.is_a?(Range)
-      Producers::RangeProducer.new(collection)
-    elsif defined?(::Queue) && collection.is_a?(::Queue)
-      Producers::QueueProducer.new(collection)
-    elsif collection.respond_to?(:lazy) && collection.is_a?(Enumerator::Lazy)
-      Producers::LazyProducer.new(collection)
-    else
-      Producers::ArrayProducer.new(collection)
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
     end
-  end
 
-  def self.build_executor(options)
-    if options[:in_processes]
-      Executors::ProcessExecutor.new(options[:in_processes])
-    else
-      Executors::ThreadExecutor.new(options[:in_threads] || 4)
+    def configure
+      yield(configuration)
+    end
+
+    def map(collection, options = {}, &block)
+      producer = wrap_producer(collection)
+      executor = build_executor(options, producer.size)
+      executor.execute(producer, &block)
+    end
+
+    def each(collection, options = {}, &block)
+      map(collection, options, &block)
+      collection
+    end
+
+    def each_with_index(collection, options = {}, &block)
+      producer = wrap_producer(collection)
+      executor = build_executor(options, producer.size)
+      executor.execute(producer) { |item| block.call(item, producer.to_a.index(item)) }
+    end
+
+    def flat_map(collection, options = {}, &block)
+      map(collection, options, &block).flatten(1)
+    end
+
+    def any?(collection, options = {}, &block)
+      cancellation = Cancellation.new
+      opts = options.merge(cancellation: cancellation)
+      found = false
+
+      map(collection, opts) do |item|
+        if block.call(item)
+          found = true
+          cancellation.cancel!
+        end
+      end
+      found
+    end
+
+    def all?(collection, options = {}, &block)
+      cancellation = Cancellation.new
+      opts = options.merge(cancellation: cancellation)
+      all_match = true
+
+      map(collection, opts) do |item|
+        unless block.call(item)
+          all_match = false
+          cancellation.cancel!
+        end
+      end
+      all_match
+    end
+
+    private
+
+    def wrap_producer(collection)
+      if collection.is_a?(Range)
+        Producers::RangeProducer.new(collection)
+      elsif defined?(::Queue) && collection.is_a?(::Queue)
+        Producers::QueueProducer.new(collection)
+      elsif collection.respond_to?(:lazy) && collection.is_a?(Enumerator::Lazy)
+        Producers::LazyProducer.new(collection)
+      else
+        Producers::ArrayProducer.new(collection)
+      end
+    end
+
+    def build_executor(options, total_size)
+      workers = options[:in_threads] || options[:in_processes] || options[:in_ractors] || configuration.default_workers
+      monitor = options[:progress] ? ProgressMonitor.new(total_size) : nil
+      cancel = options[:cancellation] || Cancellation.new
+
+      if options[:in_processes]
+        Executors::ProcessExecutor.new(workers_count: workers, progress_monitor: monitor, cancellation: cancel)
+      elsif options[:in_ractors]
+        Executors::RactorExecutor.new(workers_count: workers, progress_monitor: monitor, cancellation: cancel)
+      else
+        Executors::ThreadExecutor.new(workers_count: workers, progress_monitor: monitor, cancellation: cancel)
+      end
     end
   end
 end
-
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
-
-  def self.configure
-    yield(configuration)
-  end
-
-  def self.any?(collection, options = {}, &block)
-    cancellation = Cancellation.new
-    opts = options.merge(cancellation: cancellation)
-    found = false
-
-    map(collection, opts) do |item|
-      if block.call(item)
-        found = true
-        cancellation.cancel!
-      end
-    end
-    found
-  end
-
-  def self.all?(collection, options = {}, &block)
-    cancellation = Cancellation.new
-    opts = options.merge(cancellation: cancellation)
-    all_match = true
-
-    map(collection, opts) do |item|
-      unless block.call(item)
-        all_match = false
-        cancellation.cancel!
-      end
-    end
-    all_match
-  end
